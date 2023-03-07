2a5aec39612bf445e2083a896161585d8f1536de
Oleksandr Andriichuk <mrtproger@gmail.com>
2023-03-07 22:47:29 +0100

    feat: add dotenv and wrap all of firebase variables into env and add exampled listView

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_application_1/router/router.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
--- lib/screens/home/main_screen.dart
+++ lib/screens/home/main_screen.dart
@@ -1,29 +1,139 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/widgets.dart';
 
-class MainScreen extends StatelessWidget {
+const transactionExample = [
+  {
+    'incomeType': 'Salary',
+    'cardType': 'Debet card',
+    'transactionType': '+',
+    'transactionTime': '17:09',
+    'amount': 20,
+    'icon': Icons.arrow_forward_ios_rounded,
+    'transactionIcon': Icons.shopping_cart,
+  },
+  {
+    'incomeType': 'Salary',
+    'cardType': 'Debet card',
+    'transactionType': '+',
+    'transactionTime': '17:09',
+    'amount': 20,
+    'icon': Icons.arrow_forward_ios_rounded,
+    'transactionIcon': Icons.shopping_cart,
+  },
+  {
+    'incomeType': 'Salary',
+    'cardType': 'Debet card',
+    'transactionType': '+',
+    'transactionTime': '17:09',
+    'amount': 20,
+    'icon': Icons.arrow_forward_ios_rounded,
+    'transactionIcon': Icons.shopping_cart,
+  },
+  {
+    'incomeType': 'Salary',
+    'cardType': 'Debet card',
+    'transactionType': '+',
+    'transactionTime': '17:09',
+    'amount': 20,
+    'icon': Icons.arrow_forward_ios_rounded,
+    'transactionIcon': Icons.shopping_cart,
+  },
+];
+
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List transactionList = [];
+
+  @override
+  void initState() {
+    transactionList.addAll(transactionExample);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _iconPressHandler() {
+    final transactionData = {
+      'incomeType': 'Salary',
+      'cardType': 'Debet card',
+      'transactionType': '+',
+      'transactionTime': '17:09',
+      'amount': 20,
+      'icon': Icons.arrow_forward_ios_rounded,
+      'transactionIcon': Icons.shopping_cart,
+    };
+
+    setState(() {
+      transactionList.add(transactionData);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Center(
-        child: ListView(
-          padding: const EdgeInsets.only(top: 20.0),
-          children: const [
-            SizedBox(height: 18),
-            Text('Main Screen'),
-            TransactionItem(
-              incomeType: 'Salary',
-              cardType: 'Debet card',
-              transactionType: '+',
-              transactionTime: '17:09',
-              amount: 20,
-              icon: Icons.arrow_forward_ios_rounded,
-              transactionIcon: Icons.shopping_cart,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 18),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Add transaction',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: _iconPressHandler,
+                      icon: const Icon(
+                        Icons.add_circle_outline,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(transactionList.length.toString()),
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: transactionList.length,
+                  padding: const EdgeInsets.all(8),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TransactionItem(
+                        incomeType: transactionList[index]['incomeType'],
+                        cardType: transactionList[index]['cardType'],
+                        transactionType: transactionList[index]
+                            ['transactionType'],
+                        transactionTime: transactionList[index]
+                            ['transactionTime'],
+                        transactionIcon: transactionList[index]
+                            ['transactionIcon'],
+                        amount: transactionList[index]['amount'],
+                        icon: transactionList[index]['icon'],
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
